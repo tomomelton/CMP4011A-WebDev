bdf7a50c28e6fb9164367f64f1351eedd462cbb7
tomomelton <tomomelton@gmail.com>
2025-11-11 16:24:43 +0000

    moved the scripts for the header and footer into a seperate file and added the new link to all html files

---
+++ scripts/header-footer.js
@@ -0,0 +1,81 @@
+
+// ******************* Load Header *******************
+if (document.querySelector("header"))
+{
+    let headerElement = document.querySelector("header");
+    let jsonFile = "Header.json";
+
+    document.addEventListener("DOMContentLoaded", () =>
+    {
+        fetch(jsonFile)
+        .then(response => response.json())
+        .then(responseData =>
+            
+        {
+            for (headerData of responseData)
+            {
+                // Create Div element to hold image and title
+                let divElement = document.createElement("div");
+                let navElement = document.querySelector("nav");
+                headerElement.insertBefore(divElement, navElement)
+
+                // Create and add logo
+                let imageElement = document.createElement("img");
+                imageElement.src = headerData.imageURL;
+                imageElement.alt = headerData.alt;
+                imageElement.setAttribute("class", "logo")
+                divElement.appendChild(imageElement);
+
+                // Create and add title
+                let h1Element = document.createElement("h1");
+                h1Element.textContent = headerData.title;
+                h1Element.setAttribute("class", "title");
+                divElement.appendChild(h1Element);
+            }
+             
+            
+        }
+        )
+    }
+    )
+}
+
+// ******************* Load Footer *******************
+if (document.querySelector("footer"))
+{
+    let footerElement = document.querySelector("footer");
+    let jsonFile = "Footer.json"
+
+    document.addEventListener("DOMContentLoaded", () =>
+    {
+        fetch(jsonFile)
+        .then(response => response.json())
+        .then(responseData =>
+        {
+            for (footerData of responseData)
+            {
+                // Create Div element to hold image and footer
+                let divElement = document.createElement("div");
+                footerElement.appendChild(divElement);
+
+                // Create and add logo
+                let imageElement = document.createElement("img");
+                imageElement.src = footerData.imageURL;
+                imageElement.alt = footerData.alt;
+                imageElement.setAttribute("class","logo");
+                divElement.appendChild(imageElement);
+
+                // Create and add footer text
+                let pElement = document.createElement("p");
+                pElement.textContent = footerData.footer;
+                divElement.appendChild(pElement);
+            }
+            
+        }
+        )
+    }
+    )
+
+    
+
+}
