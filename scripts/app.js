3ccb34080773bc0ff639fc8a8619d60103bd1727
tomomelton <tomomelton@gmail.com>
2025-11-11 15:57:18 +0000

    added dynamic loading for header content

--- scripts/app.js
+++ scripts/app.js
@@ -9,3 +9,45 @@ document.addEventListener("DOMContentLoaded", () => {
     });
    
 });
+
+
+// ******************* Load Header *******************
+if (document.querySelector("header"))
+{
+    let headerElement = document.querySelector("header");
+    let jsonFile = "Header.json";
+
+    document.addEventListener("DOMContentLoaded", () =>
+    {
+        fetch(jsonFile)
+        .then(response => response.json())
+        .then(responseData =>
+            
+        {
+            for (headerData of responseData)
+            {
+                // Create Div element to hold image and title
+                let divElement = document.createElement("div");
+                let navElement = document.querySelector("nav");
+                headerElement.insertBefore(divElement, navElement)
+
+                // Create and add logo
+                let imageElement = document.createElement("img");
+                imageElement.src = headerData.imageURL;
+                imageElement.alt = headerData.alt;
+                imageElement.setAttribute("class", "logo")
+                divElement.appendChild(imageElement);
+
+                // Create and add title
+                let h1Element = document.createElement("h1");
+                h1Element.textContent = headerData.title;
+                h1Element.setAttribute("class", "title");
+                divElement.appendChild(h1Element);
+            }
+             
+            
+        }
+        )
+    }
+    )
+}
