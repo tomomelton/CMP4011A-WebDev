d9302b4bea7501443e675227674f3237bda58b0f
Christian <zgd25pqu@uea.ac.uk>
2025-11-17 22:49:40 +0000

    Remove DOMContentLoaded event listener
    It is no longer necesary since the drop down menu works with css

--- scripts/app.js
+++ scripts/app.js
@@ -1,12 +1,2 @@
-document.addEventListener("DOMContentLoaded", () => {
-    // Deletes class 'no-js' from the body if JS is active 
-    document.body.classList.remove("no-js");
 
-    link.addEventListener("click", (e) => {
-        // Avoids reloading page
-        e.preventDefault();
-        submenu.style.display = submenu.style.display === "block" ? "none" : "block";
-    });
-   
-});
 
