5ff23b1833da4e41231ea8d4c81ace04fa602e3a
tomomelton <tomomelton@gmail.com>
2025-11-18 14:49:46 +0000

    Finished Node.JS and Express setup

--- app/app.js
+++ app/app.js
@@ -2,11 +2,10 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
-app.use(express.static('public'));
+app.use(express.urlencoded({extended: true}))
+app.use(express.json())
 
-app.get("/", (req, res) => {
-res.send("Hello, welcome to my NodeJS app!");
-});
+app.use(express.static('public'));
 
 app.listen(port, () => {
 console.log(`myapp is listening on port ${port}!`);
