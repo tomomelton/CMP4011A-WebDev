396294fa4412c18d8867ad3618c466e20eb66e56
Henry Barnes <henrytbarnes07@gmail.com>
2025-12-04 11:26:23 +0000

    image carosuel added

--- app/public/scripts/hunger.js
+++ app/public/scripts/hunger.js
@@ -19,10 +19,24 @@ fetch(jsonFile)
       let imageCarouselData = data[0].imageCarousel;
 
       imageCarouselElement.innerHTML = `
-                <img src="${imageCarouselData.img1}" />
-                <img src="${imageCarouselData.img2}" />
-                <img src="${imageCarouselData.img3}" />
-            `;
+      <div class="mySlides">
+        <img src="${imageCarouselData.img1}" style="width:100%">
+      </div>
+
+      <div class="mySlides">
+        <img src="${imageCarouselData.img2}" style="width:100%">
+      </div>
+
+      <div class="mySlides">
+        <img src="${imageCarouselData.img3}" style="width:100%">
+      </div>
+
+      <a class="prev" onclick="plusSlides(-1)">❮</a>
+      <a class="next" onclick="plusSlides(1)">❯</a>
+    `;
+
+      slideIndex = 1;
+      showSlides(slideIndex);
     }
 
     // Overview text
@@ -63,3 +77,28 @@ fetch(jsonFile)
     }
   })
   .catch((err) => console.error("Error loading JSON:", err));
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides((slideIndex += n));
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+
+  slides[slideIndex - 1].style.display = "block";
+}
